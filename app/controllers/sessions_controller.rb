16a98355ad1162b4e4bdbb24d3f7520ed3503f9a
MustafaSKaya <mustafakya88@hotmail.com>
2022-02-09 20:02:19 +0000

    user test are added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,12 @@ class SessionsController < ApplicationController
     end
 
     def create
-      user = User.find_by_email(params[:email])
-      # If the user exists AND the password entered is correct.
-      puts "Mustafa #{params[:password]}"
-
-      if user && user.authenticate(params[:password])
-        puts user
-        # Save the user id inside the browser cookie. This is how we keep the user 
-        # logged in when they navigate around our website.
+      if user = User.authenticate_with_credentials(params[:email].strip, params[:password])
+        # success logic, log them in
         session[:user_id] = user.id
         redirect_to '/'
       else
-      # If user's login doesn't work, send them back to the login form.
+        # failure, render login form
         redirect_to '/login'
       end
     end
